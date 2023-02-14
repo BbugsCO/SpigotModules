cb2eb07ee9f3dc30044efb1a1f4d92b57afa2d8f
BbugsCO <76406123+BbugsCO@users.noreply.github.com>
2023-02-14 09:40:53 -0700

    Update Utils.java

--- Utils.java
+++ Utils.java
@@ -1,5 +1,4 @@
 package domain.name.plugin;
-
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 
@@ -7,29 +6,57 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Utils {
-    // Converts String value to Location (format:x-y-z-world)
-    public Location stringToLocation(String s) {
-        ArrayList<String> position = new ArrayList<String>(List.of(s.split("-")));
+    /**
+     * Converts a string to location
+     * format must be:  "x-y-z-world"
+     *
+     * @param location string value of a location
+     *
+     * @return Location
+     */
+    public Location stringToLocation(String location) {
+        ArrayList<String> position = new ArrayList<>(List.of(location.split("-")));
+        if (position.size() != 4) { return null; }
         return new Location(Bukkit.getWorld(position.get(3)), Double.parseDouble(position.get(0)), Double.parseDouble(position.get(1)), Double.parseDouble(position.get(2)));
     }
 
-
-    // Convert Location to String (format:x-y-z-world)
-    public String locationToString(Location l) {
-        return l.getBlockX() + "-" + l.getBlockY() + "-" + l.getBlockZ() + "-" + l.getWorld().toString();
+    /**
+     * Converts a location
+     * formatted as "x-y-z-world"
+     *
+     * @param location location
+     *
+     * @return Formatted string of location
+     */
+    public String locationToString(Location location) {
+        return location.getBlockX() + "-" + location.getBlockY() + "-" + location.getBlockZ() + "-" + location.getWorld().toString();
     }
 
-    // Tests to see if String is numeric
-    public Boolean isNumeric(String s) {
+
+    /**
+     * Tests to see if a string value is numeric
+     *
+     * @param value string to test if numeric
+     *
+     * @return Bool
+     */
+    public Boolean isNumeric(String value) {
         try {
-            Double d = Double.parseDouble(s);
+            Double d = Double.parseDouble(value);
         } catch (NumberFormatException e) {
             return false;
         }
         return true;
     }
-    
-    // Generate a random integer in range [min, max]
+
+    /**
+     * Generates random number
+     * 
+     * @param min minimum number in range
+     * @param max maximum number in range
+     *
+     * @return random int
+     */
     public int randomNumber(int min, int max) {
         return (int)(Math.random() * ((max - min) + 1)) + min;
     }
